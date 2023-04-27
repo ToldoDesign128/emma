c9d56df1ce099d2e2cbdc6558006306bc2df2fb0
Federico <fede.toldo96@gmail.com>
2023-04-27 16:40:18 +0200

    Items content
    Items content

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="contact">
         <div class="contact__items"><p>E-mail | <a href="mailto:emmascala1@live.it" class="contact__items__link">emmascala1@live.it</a></p></div>
-        <div class="contact__items"><p>Il mio curriculum | <a href="mailto:emmascala1@live.it" class="contact__items__link">scarica qui</a></p></div>
+        <div class="contact__items"><p>Il mio curriculum | <a href="http://sergiom109.sg-host.com/wp-content/uploads/2023/04/CV_Emma_design_IT_compressed.pdf" class="contact__items__link">scarica qui</a></p></div>
         <div class="contact__items"><p>Instagram | <a href="https://www.instagram.com/emmascala" class="contact__items__link">@emmascala</a></p></div>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -1,104 +1,175 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
-<section class="home">
-    <div class="home__text">
-        <h1>Ciao, mi chiamo Emma.</h1>
-        <p>Sono una visual designer e questi sono alcuni</p>
-        <p>dei miei lavori. I miei amici mi hanno chiesto di disegnare una</p> 
-        <p> <a href="" class="home__text__link"> maglietta.</a> Abbiamo anche organizzato un <a href="" class="home__text__link">risotto party.</a> La mia ricerca</p>
-        <p> sull'affascinante mondo dei colori ha dato frutto ad un <a href="" class="home__text__link">libro</a>,un <a href="" class="home__text__link">poster</a>,</p>
-        <p> alcune <a href="" class="home__text__link">cartoline</a> e un <a href="" class="home__text__link">installazione audiovisiva</a></p>
-        <p>Come ultima cosa, ho collaborato con</p>
-        <p> un negozio di vestiti magico.</p>
-    </div>
-</section>
-<section class="home__mobile">
-    <div class="home__mobile__text">
-        <p>Ciao, mi chiamo Emma. <br> Sono una visual designer e questi sono alcuni dei miei lavori. I miei amici mi hanno chiesto di disegnare una <a href=""> maglietta.</a> Abbiamo anche organizzato un <a href="">risotto party.</a> La mia ricerca sull'affascinante mondo dei colori ha dato frutto ad un <a href="">libro</a>,un <a href="">poster</a>, alcune <a href="">cartoline</a> e un <a href="">installazione audiovisiva.</a> Come ultima cosa, ho collaborato con un negozio di vestiti magico.</p>
-    </div>
-</section>
-<section class="animation">
-    <div class="animation__box">
-        <div class="animation__box__1"> <h5>grafica editoriale | brand identity | promozione eventi | installazioni</h5> </div>
-        <div class="animation__box__2"> <h5>BA | Design del prodotto e della comunicazione | IUAV Università di Venezia</h5> </div>
-        <div class="animation__box__3"> <h5>MA | Visual Communication | Zürcher Hochschule der Künste</h5> </div>
-        <div class="animation__box__4"> <h5>Vivo a Mori ma al momento mi trovo a Zurigo.</h5> </div>
-    </div>
-</section>
+<main>
 
-<!-- Maglietta -->
-<section class="maglietta">
-    <div class="maglietta__text">
-        <h2 class="maglietta__text__title">Maglietta | IBDS</h2>
-        <p>Questa maglietta nasce dall’idea di identificare un gruppo di amici che si conosce ormai da anni.</p>
-        <p>Cosa gli accomuna? Il Campari naturalmente.</p>
-        <p>Giugno 2022</p>
-    </div>
-    <div class="maglietta__image">
-        <img src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
-        <img src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
-        <img src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
-    </div>
-</section>
+    <section class="home">
+        <div class="home__text">
+            <h1>Ciao, mi chiamo Emma.</h1>
+            <p>Sono una visual designer e questi sono alcuni</p>
+            <p>dei miei lavori. I miei amici mi hanno chiesto di disegnare una</p>
+            <p> <a href="#" class="home__text__link maglietta-toggle"> maglietta.</a> Abbiamo anche organizzato un <a href="#" class="home__text__link toggle">risotto party.</a> La mia ricerca</p>
+            <p> sull'affascinante mondo dei colori ha dato frutto ad un <a href="#" class="home__text__link toggle">libro</a>,un <a href="#" class="home__text__link toggle">poster</a>,</p>
+            <p> alcune <a href="#" class="home__text__link toggle">cartoline</a> e un <a href="#" class="home__text__link toggle">installazione audiovisiva</a></p>
+            <p>Come ultima cosa, ho collaborato con</p>
+            <p> un negozio di vestiti magico.</p>
+        </div>
+    </section>
+    <section class="home__mobile">
+        <div class="home__mobile__text">
+            <p>Ciao, mi chiamo Emma. <br> Sono una visual designer e questi sono alcuni dei miei lavori. I miei amici mi hanno chiesto di disegnare una <a href="#maglietta__mobile"> maglietta.</a> Abbiamo anche organizzato un <a href="#risotto__mobile">risotto party.</a> La mia ricerca sull'affascinante mondo dei colori ha dato frutto ad un <a href="#">libro</a>,un <a href="#">poster</a>, alcune <a href="#">cartoline</a> e un <a href="#">installazione audiovisiva.</a> Come ultima cosa, ho collaborato con un negozio di vestiti magico.</p>
+        </div>
+    </section>
+    <section class="animation">
+        <div class="animation__box">
+            <div class="animation__box__1">
+                <h5>grafica editoriale | brand identity | promozione eventi | installazioni</h5>
+            </div>
+            <div class="animation__box__2">
+                <h5>BA | Design del prodotto e della comunicazione | IUAV Università di Venezia</h5>
+            </div>
+            <div class="animation__box__3">
+                <h5>MA | Visual Communication | Zürcher Hochschule der Künste</h5>
+            </div>
+            <div class="animation__box__4">
+                <h5>Vivo a Mori ma al momento mi trovo a Zurigo.</h5>
+            </div>
+        </div>
+    </section>
 
-<!-- Risotto -->
-<section class="risotto">
-    <div class="risotto__text">
-        <h2 class="risotto__text__title">RIOSTTO PARTY | RISOTTATA DAL ROMANO</h2>
-        <p>In un momento conviviale cosa c'è di meglio di una forchettata di risotto e un bicchiere di vino?</p>
-        <p>Per esaltare le prelibatezze della tradizione culinaria veneta, ho utilizzato un visual astratto e moderno che allude alla pietanza e all'effetto provocato dal vino.</p>
-        <p>Agosto 2022</p>
-    </div>
-</section>
+    <!-- Maglietta -->
+    <section class="maglietta toggle-items">
+        <div class="maglietta__text">
+            <h2 class="maglietta__text__title">Maglietta | IBDS</h2>
+            <p>Questa maglietta nasce dall’idea di identificare un gruppo di amici che si conosce ormai da anni.</p>
+            <p>Cosa gli accomuna? Il Campari naturalmente.</p>
+            <p>Giugno 2022</p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
 
-<!-- Libro -->
-<section class="libro">
-    <div class="libro__text">
-        <h2 class="libro__text__title"></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
-</section>
+    <!-- Maglietta Mobile -->
+    <section id="maglietta__mobile" class="maglietta__mobile toggle-items">
+        <div class="maglietta__mobile__text">
+            <h2 class="maglietta__mobile__text__title">Maglietta__mobile | IBDS</h2>
+            <p>Questa maglietta__mobile nasce dall’idea di identificare un gruppo di amici che si conosce ormai da anni. Cosa gli accomuna? Il Campari naturalmente.</p>
+            <p>Giugno 2022</p>
+        </div>
+        <div class="maglietta__mobile__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="">
+        </div>
+    </section>
 
-<!-- Poster -->
-<section class="poster">
-    <div class="poster__text">
-        <h2 class="poster__text__title"></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
-</section>
+    <!-- Risotto -->
+    <section class="risotto toggle-items">
+        <div class="risotto__text">
+            <h2 class="risotto__text__title">RIOSTTO PARTY | RISOTTATA DAL ROMANO</h2>
+            <p>In un momento conviviale cosa c'è di meglio di una forchettata di risotto e un bicchiere di vino?</p>
+            <p>Per esaltare le prelibatezze della tradizione culinaria veneta, ho utilizzato un visual astratto e moderno che allude alla pietanza e all'effetto provocato dal vino.</p>
+            <p>Agosto 2022</p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
 
-<!-- Cartoline -->
-<section class="cartoline">
-    <div class="cartoline__text">
-        <h2 class="cartoline__text__title"></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
-</section>
+    <!-- Risotto Mobile -->
+    <section id="risotto__mobile" class="risotto__mobile toggle-items">
+        <div class="risotto__mobile__text">
+            <h2 class="risotto__mobile__text__title">RIOSTTO PARTY | RISOTTATA DAL ROMANO</h2>
+            <p>In un momento conviviale cosa c'è di meglio di una forchettata di risotto e un bicchiere di vino?</p>
+            <p>Per esaltare le prelibatezze della tradizione culinaria veneta, ho utilizzato un visual astratto e moderno che allude alla pietanza e all'effetto provocato dal vino.</p>
+            <p>Agosto 2022</p>
+        </div>
+        <div class="risotto__mobile__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="">
+        </div>
+    </section>
 
-<!-- Video -->
-<section class="video">
-    <div class="video__text">
-        <h2 class="video__text__title"></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
-</section>
+    <!-- Libro -->
+    <section class="libro toggle-items">
+        <div class="libro__text">
+            <h2 class="libro__text__title"></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
 
-<!-- Vestiti -->
-<section class="vestiti">
-    <div class="vestiti__text">
-        <h2 class="vestiti__text__title"></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-    </div>
-</section>
+    <!-- Poster -->
+    <section class="poster toggle-items">
+        <div class="poster__text">
+            <h2 class="poster__text__title"></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
 
-<?php get_footer();?>
+    <!-- Cartoline -->
+    <section class="cartoline toggle-items">
+        <div class="cartoline__text">
+            <h2 class="cartoline__text__title"></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
+
+    <!-- Video -->
+    <section class="video toggle-items">
+        <div class="video__text">
+            <h2 class="video__text__title">EXHIBITION | SPEAKING THE SEEN</h2>
+            <p>Il colore è una questione di sensazioni, sentimenti, ricordi e connessioni logiche.</p>
+            <p>L'esplorazione lo separa in due livelli di astrazione: uno riguarda le parole e il secondo le immagini. In questa mostra le fotografie sono utilizzate per illustrare e visualizzare i colori, mentre le parole sono il frutto di una prospettiva individuale ambigua e non univoca.</p>
+            <p>Le immagini sono state scattate nella città di Zurigo e simboleggiano l'ordinaria scena dei colori, mentre l'audio letto dall'intelligenza artificiale rappresenta l'arbitrarietà della denominazione dei colori.</p>
+            <p>La combinazione di questi due livelli evidenzia come la percezione dell'osservatore cambia ed influenzi la scelta dei nomi dei colori.</p>
+            <p>Zurigo, dicembre 2022</p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
+
+    <!-- Vestiti -->
+    <section class="vestiti toggle-items">
+        <div class="vestiti__text">
+            <h2 class="vestiti__text__title"></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div class="maglietta__image">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__1">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__2">
+            <img loading="lazy" src="http://emma.local/wp-content/uploads/2023/04/maglietta-scaled.jpg" alt="" class="maglietta__image__3">
+        </div>
+    </section>
+
+</main>
+
+<?php get_footer(); ?>
